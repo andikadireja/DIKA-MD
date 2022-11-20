38e35e0fdeaabb877a67fdeffa46938f54ff0e2c
andikadireja <111603873+andikadireja@users.noreply.github.com>
2022-11-20 13:19:15 +0700

    Update help.js

--- help.js
+++ help.js
@@ -5,7 +5,7 @@
 var monoSpace = '```'
 
 exports.listmenu = (sender,prefix,ad,namenya,premnya,usernya,romnya,tanggal,jam) => {
-return`━━━❲ 𝙂𝙐𝙍𝘽𝙊𝙏-𝙈𝘿 ❳━━━
+return`━━━❲ Dikaa Channel ❳━━━
 ┏━━━━━━━━━━━━━
 ┣━( 𝗨𝗦𝗘𝗥 𝗜𝗡𝗙𝗢 )
 ┃▤ ID : @${sender.split('@')[0]}
@@ -723,13 +723,10 @@ return`──「 *MENU DONATE* 」──
 
 Hi *${cekName}* ${ucapanWaktu} 👋🏻
 
-*Payment Ovo*
-Number: 085789004732
-A/N: LEXXY STORE
 
 *Payment Dana*
-Number: 085789004732
-A/N: LEXXY OFFICIAL
+Number: 089665988228
+A/N: Dikaa store
 
 ${monoSpace}Terimakasih untuk kamu yang sudah donasi untuk perkembangan bot ini ^_^${monoSpace}
 
@@ -740,17 +737,17 @@ exports.infoOwner = () =>{
 return`──「 *INFO OWNER* 」──
 
  *Data Profil*
- • *Nama:* Lexxy Official
- • *Umur:* 18 tahun
- • *Hoby:* Turu/Game
- • *Askot:* Palembang
+ • *Nama:* andika
+ • *Umur:* 19 tahun
+ • *Hoby:* cari cuan
+ • *Askot:* karawang
  • *Konten:* Creator
 
 _iam developer bot whatsapp._
 
  *Sosial Media*
  • *Whatsapp:* 083834558105
- • *Youtube:* Lexxy Official
- • *Github:* Lexxy24
+ • *Youtube:* Dikaa channel v7
+ • *Github:* andikadireja
  `
 }
